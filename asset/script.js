14b6a1a9714ca37f5164ac1d6b741e2afbf8d018
Badr Almadhi <badr.almadhi95@gmail.com>
2023-12-18 21:41:40 -0700

    added JS to make web responsive

--- asset/script.js
+++ asset/script.js
@@ -1,22 +1,44 @@
 // declare a variables from HTML
-var userInput = document.getElementById("input");
-var searchBtn = document.querySelector("search-btn");
-var citiesNames = document.querySelector("cities"); //ul add list in it
-var forecastContainer = document.querySelector("forecast-container");
+var searchBtn = document.querySelector(".search-btn");
+var cityInput = document.getElementById("city-input");
 
-
+// var cityName = cityInput.value.trim();
 // API Key
-var apiKey = `76bed644bbb6bf7b66cb2167b944672f`;
-var city = document.getElementById("input").value;
-// API URL with the query parameter
-var queryURl = `https://api.openweathermap.org/data/2.5/weather?q=${saltlake}&appid=${apiKey}`;
+var apiKey = `544df32abd3d80b6cd05d312a48dfefd`;
 
-fetch(queryURl).then(function (response) {
-  console.log(response);
-  if (response.ok) {
-    response.json().then(function (data) {
+const getWeatherDetails = (cityName, lat, lon) => {
+  debugger
+  const API_URL_Forecast = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  debugger
+  fetch(API_URL_Forecast)
+    .then((res) => res.json())
+    .then((data) => {
+      debugger
       console.log(data);
+    })
+    .catch(() => {
+      alert("Error occurred while fetching for weather forecast");
     });
-  }
-});
+};
 
+const getCityCoordinate = (event) => {
+  event.preventDefault(); // prevent default to lose info
+  debugger
+  const cityName = cityInput.value.trim(); // git red of extra spaces
+  if (!cityName) return; // return if cityName is empty
+  const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+
+  // This fetch gets the city coordinate (latitude, longitude, and name) from API JSON data
+  fetch(apiURL)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.length) return alert(`No coordinate found for ${cityName}`);
+      const { name, lat, lon } = data[0]; // in json data all info needed is under data[0];
+      getWeatherDetails(name, lat, lon);
+    })
+    .catch(() => {
+      alert("Error occurred while fetching for coordinate");
+    });
+};
+
+searchBtn.addEventListener("click", getCityCoordinate);
